d8f4b085d36da5e33d4b4c013ee2acb64bde47b1
Nathan Stoltenberg <stoltnate@gmail.com>
2025-03-30 09:36:08 -0700

    Add conditional rendering for remove button in AddTradeModalComponent

--- ClientApp/src/app/add-trade-modal/add-trade-modal.component.ts
+++ ClientApp/src/app/add-trade-modal/add-trade-modal.component.ts
@@ -3,12 +3,12 @@ import { FormBuilder, FormGroup, Validators, ReactiveFormsModule, FormArray } fr
 import { TradeService } from '../trade.service';
 import { Trade, TradeEntry } from '../shared/models/trade.model';
 import { QuillModule } from 'ngx-quill';
-import { NgFor } from '@angular/common';
+import { NgFor, NgIf } from '@angular/common';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-add-trade-modal',
-  imports: [ReactiveFormsModule, QuillModule, NgFor],
+  imports: [ReactiveFormsModule, QuillModule, NgFor, NgIf],
   standalone: true,
   templateUrl: './add-trade-modal.component.html',
   styleUrl: './add-trade-modal.component.css'
