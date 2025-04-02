44891f90a4e3780b51f4268a1b602e9e0ccdd7cd
Nathan Stoltenberg <stoltnate@gmail.com>
2025-04-02 15:15:09 -0700

    fix tab colors not changing on click

--- ClientApp/src/app/add-trade-modal/tab/tab.component.ts
+++ ClientApp/src/app/add-trade-modal/tab/tab.component.ts
@@ -1,14 +1,17 @@
+import { NgClass } from '@angular/common';
 import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
     selector: 'addt-modal-tab',
+    imports: [NgClass],
     templateUrl: './tab.component.html',
 })
 export class TabComponent {
     @Output() switchTabEvent = new EventEmitter<number>();
+    activeTab = 0;
 
     activateTab(tab: number) {
-        console.log("Emitting: ", tab)
+        this.activeTab = tab;
         this.switchTabEvent.emit(tab);
     }
 }
