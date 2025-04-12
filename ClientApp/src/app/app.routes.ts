0fa9f48f37c311284b38142d78f8b22cbde64fbb
Nathan Stoltenberg <stoltnate@gmail.com>
2025-04-12 13:47:57 -0700

    add tool-menu compontent and path

--- ClientApp/src/app/app.routes.ts
+++ ClientApp/src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { TradeListComponent } from './trade-list/trade-list.component';
+import { ToolMenuComponent } from './tool-menu/tool-menu.component';
 
 export const routes: Routes = [
-    {
-        path: '',
-        component: TradeListComponent
-    }
+  {
+    path: '',
+    component: TradeListComponent,
+  },
+  {
+    path: 'tools',
+    component: ToolMenuComponent,
+  },
 ];
---
+++ ClientApp/src/app/tool-menu/tool-menu.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tool-menu',
+  imports: [],
+  templateUrl: './tool-menu.component.html',
+  styleUrl: './tool-menu.component.css'
+})
+export class ToolMenuComponent {
+
+}
