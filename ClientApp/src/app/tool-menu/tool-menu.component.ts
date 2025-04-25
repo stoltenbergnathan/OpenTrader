4542e6046dc4fe49395fd3ee7ab811eea8476b01
Nathan Stoltenberg <stoltnate@gmail.com>
2025-04-25 08:04:03 -0700

    cleaned up tool selection

--- ClientApp/src/app/tool-menu/tool-menu.component.ts
+++ ClientApp/src/app/tool-menu/tool-menu.component.ts
@@ -1,12 +1,17 @@
 import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
+interface Tool {
+  path: string;
+  label: string;
+}
 @Component({
   selector: 'app-tool-menu',
-  imports: [NgFor, RouterLink, RouterOutlet],
+  standalone: true,
+  imports: [NgFor, RouterLink, RouterOutlet, RouterLinkActive],
   templateUrl: './tool-menu.component.html',
 })
 export class ToolMenuComponent {
-  tools: string[] = ['option-stop-loss'];
+  tools: Tool[] = [{ path: 'option-stop-loss', label: 'Option Stop Loss' }];
 }
