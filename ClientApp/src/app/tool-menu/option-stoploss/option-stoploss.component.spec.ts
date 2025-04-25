ecb6592b1035ce31399fbf81c48a844cd7345fe7
Nathan Stoltenberg <stoltnate@gmail.com>
2025-04-25 09:16:34 -0700

    test outline

--- ClientApp/src/app/tool-menu/option-stoploss/option-stoploss.component.spec.ts
+++ ClientApp/src/app/tool-menu/option-stoploss/option-stoploss.component.spec.ts
@@ -8,9 +8,8 @@ describe('OptionStoplossComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [OptionStoplossComponent]
-    })
-    .compileComponents();
+      imports: [OptionStoplossComponent],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(OptionStoplossComponent);
     component = fixture.componentInstance;
@@ -19,5 +18,25 @@ describe('OptionStoplossComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('stopsAbove', () => {
+    it('should return an empty array when optionPrice is not valid', () => {});
+    it('should return values in correct order (highest stop first)', () => {});
+    it('should calculate values correctly', () => {});
+    it('should return an array with one value when incrementsToShow is set to 1', () => {});
+  });
+
+  describe('stopsBelow', () => {
+    it('should return an empty array when optionPrice is not valid', () => {});
+    it('should return values in correct order (lowest stop first)', () => {});
+    it('should calculate values correctly', () => {});
+    it('should return an array with one value when incrementsToShow is set to 1', () => {});
+  });
+
+  describe('hasValidOptionPrice', () => {
+    it('should return false when option price is 0', () => {});
+    it('should return false when option price is negative', () => {});
+    it('should return true when option price is positive', () => {});
   });
 });
