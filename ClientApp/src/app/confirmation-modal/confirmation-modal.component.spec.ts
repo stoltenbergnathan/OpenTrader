1a1f72ebf122bda3552482ec7f189bd14ac7298a
Nathan Stoltenberg <stoltnate@gmail.com>
2025-04-27 07:45:29 -0700

    get confirmation modal passing

--- ClientApp/src/app/confirmation-modal/confirmation-modal.component.spec.ts
+++ ClientApp/src/app/confirmation-modal/confirmation-modal.component.spec.ts
@@ -1,16 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ConfirmationModalComponent } from './confirmation-modal.component';
+import { MatDialogRef } from '@angular/material/dialog';
 
 describe('ConfirmationModalComponent', () => {
   let component: ConfirmationModalComponent;
   let fixture: ComponentFixture<ConfirmationModalComponent>;
+  let matDialogRefMock = jasmine.createSpyObj('MatDialogRef', ['close']);
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ConfirmationModalComponent]
-    })
-    .compileComponents();
+      imports: [ConfirmationModalComponent],
+      providers: [{ provide: MatDialogRef, useValue: matDialogRefMock }],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(ConfirmationModalComponent);
     component = fixture.componentInstance;
