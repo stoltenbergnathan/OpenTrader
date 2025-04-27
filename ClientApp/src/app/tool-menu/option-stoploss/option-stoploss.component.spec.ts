248abffe0d0e28e14cfcdce9244e031fd5fcec1f
Nathan Stoltenberg <stoltnate@gmail.com>
2025-04-27 07:29:14 -0700

    add tests for component

--- ClientApp/src/app/tool-menu/option-stoploss/option-stoploss.component.spec.ts
+++ ClientApp/src/app/tool-menu/option-stoploss/option-stoploss.component.spec.ts
@@ -21,22 +21,106 @@ describe('OptionStoplossComponent', () => {
   });
 
   describe('stopsAbove', () => {
-    it('should return an empty array when optionPrice is not valid', () => {});
-    it('should return values in correct order (highest stop first)', () => {});
-    it('should calculate values correctly', () => {});
-    it('should return an array with one value when incrementsToShow is set to 1', () => {});
+    it('should return an empty array when optionPrice is not valid', () => {
+      component.optionPrice = 0;
+      component.stopLossIncrements = 5;
+      component.incrementsToShow = 2;
+
+      var stopsAbove = component.stopsAbove;
+
+      expect(stopsAbove.length).toBe(0);
+    });
+    it('should return values in decending order', () => {
+      component.optionPrice = 1;
+      component.stopLossIncrements = 5;
+      component.incrementsToShow = 2;
+
+      var stopsAbove = component.stopsAbove;
+
+      expect(stopsAbove[0]).toBeGreaterThan(stopsAbove[1]);
+    });
+    it('should calculate values correctly', () => {
+      component.optionPrice = 1;
+      component.stopLossIncrements = 5;
+      component.incrementsToShow = 2;
+
+      var stopsAbove = component.stopsAbove;
+
+      expect(stopsAbove[0]).toBe(1.1);
+      expect(stopsAbove[1]).toBe(1.05);
+    });
+    it('should return an array with one value when incrementsToShow is set to 1', () => {
+      component.optionPrice = 1;
+      component.stopLossIncrements = 5;
+      component.incrementsToShow = 1;
+
+      var stopsAbove = component.stopsAbove;
+
+      expect(stopsAbove.length).toBe(1);
+    });
   });
 
   describe('stopsBelow', () => {
-    it('should return an empty array when optionPrice is not valid', () => {});
-    it('should return values in correct order (lowest stop first)', () => {});
-    it('should calculate values correctly', () => {});
-    it('should return an array with one value when incrementsToShow is set to 1', () => {});
+    it('should return an empty array when optionPrice is not valid', () => {
+      component.optionPrice = 0;
+      component.stopLossIncrements = 5;
+      component.incrementsToShow = 1;
+
+      var stopsBelow = component.stopsBelow;
+
+      expect(stopsBelow.length).toBe(0);
+    });
+    it('should return values in decending order', () => {
+      component.optionPrice = 1;
+      component.stopLossIncrements = 5;
+      component.incrementsToShow = 2;
+
+      var stopsBelow = component.stopsBelow;
+
+      expect(stopsBelow[0]).toBeGreaterThan(stopsBelow[1]);
+    });
+    it('should calculate values correctly', () => {
+      component.optionPrice = 1;
+      component.stopLossIncrements = 5;
+      component.incrementsToShow = 2;
+
+      var stopsBelow = component.stopsBelow;
+
+      expect(stopsBelow[0]).toBe(0.95);
+      expect(stopsBelow[1]).toBe(0.9);
+    });
+    it('should return an array with one value when incrementsToShow is set to 1', () => {
+      component.optionPrice = 1;
+      component.stopLossIncrements = 5;
+      component.incrementsToShow = 1;
+
+      var stopsBelow = component.stopsBelow;
+
+      expect(stopsBelow.length).toBe(1);
+    });
   });
 
   describe('hasValidOptionPrice', () => {
-    it('should return false when option price is 0', () => {});
-    it('should return false when option price is negative', () => {});
-    it('should return true when option price is positive', () => {});
+    it('should return false when option price is 0', () => {
+      component.optionPrice = 0;
+
+      let isValidOptionPrice = component.hasValidOptionPrice();
+
+      expect(isValidOptionPrice).toBe(false);
+    });
+    it('should return false when option price is negative', () => {
+      component.optionPrice = -1;
+
+      let isValidOptionPrice = component.hasValidOptionPrice();
+
+      expect(isValidOptionPrice).toBe(false);
+    });
+    it('should return true when option price is positive', () => {
+      component.optionPrice = 1;
+
+      let isValidOptionPrice = component.hasValidOptionPrice();
+
+      expect(isValidOptionPrice).toBe(true);
+    });
   });
 });
