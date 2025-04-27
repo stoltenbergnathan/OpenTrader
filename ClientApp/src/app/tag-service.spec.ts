08848e3ce9d083e13593cf517edefe1cda74a972
Nathan Stoltenberg <stoltnate@gmail.com>
2025-04-27 08:13:20 -0700

    fix tag service tests

--- ClientApp/src/app/tag-service.spec.ts
+++ ClientApp/src/app/tag-service.spec.ts
@@ -1,12 +1,17 @@
 import { TestBed } from '@angular/core/testing';
 
 import { TagService } from './tag-service';
+import { HttpClient } from '@angular/common/http';
 
 describe('TagService', () => {
   let service: TagService;
+  let httpClientSpy: jasmine.SpyObj<HttpClient>;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    httpClientSpy = jasmine.createSpyObj('HttpClient', ['get']);
+    TestBed.configureTestingModule({
+      providers: [TagService, { provide: HttpClient, useValue: httpClientSpy }],
+    });
     service = TestBed.inject(TagService);
   });
 
