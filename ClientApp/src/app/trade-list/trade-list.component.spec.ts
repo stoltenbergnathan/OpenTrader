066b14592fe215d55568a36290b11b9591a0f67a
Nathan Stoltenberg <stoltnate@gmail.com>
2025-04-27 08:00:48 -0700

    stop trade list component from failing

--- ClientApp/src/app/trade-list/trade-list.component.spec.ts
+++ ClientApp/src/app/trade-list/trade-list.component.spec.ts
@@ -1,19 +1,34 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TradeListComponent } from './trade-list.component';
+import { BehaviorSubject, EMPTY, of } from 'rxjs';
+import { TradeEntry } from '../shared/models/trade.model';
+import { TradeService } from '../trade.service';
+
+class MockTradeService {
+  tradeEntries$ = new BehaviorSubject<TradeEntry[]>([]);
+
+  getTrades = jasmine.createSpy('getTrades').and.returnValue(of(EMPTY));
+}
 
 describe('TradeListComponent', () => {
   let component: TradeListComponent;
   let fixture: ComponentFixture<TradeListComponent>;
+  let mockTradeService: MockTradeService;
 
   beforeEach(async () => {
+    const tradeServiceInstance = new MockTradeService();
+
     await TestBed.configureTestingModule({
-      imports: [TradeListComponent]
-    })
-    .compileComponents();
+      imports: [TradeListComponent],
+      providers: [{ provide: TradeService, useValue: tradeServiceInstance }],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(TradeListComponent);
     component = fixture.componentInstance;
+    mockTradeService = TestBed.inject(
+      TradeService
+    ) as unknown as MockTradeService;
     fixture.detectChanges();
   });
 
