ea5a943f33413f9d3c9775aafa5f1d1f5ac7ea49
Nathan Stoltenberg <stoltnate@gmail.com>
2025-04-27 09:11:01 -0700

    app copmonet tests passing

--- ClientApp/src/app/app.component.ts
+++ ClientApp/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, HostListener } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { TopNavbarComponent } from './top-navbar/top-navbar.component';
@@ -6,6 +6,7 @@ import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   imports: [RouterOutlet, NavMenuComponent, TopNavbarComponent, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
